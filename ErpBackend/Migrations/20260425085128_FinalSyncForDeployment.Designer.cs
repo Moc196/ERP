8facb1d68deeab30fbad514aca04703c34c5a6c0
do dat <dodat1962004@gmail.com>
2026-04-25 15:51:28 +0700

    Add final migration and suppress pending changes warning

--- ErpBackend/Migrations/20260425085128_FinalSyncForDeployment.Designer.cs
+++ ErpBackend/Migrations/20260425085128_FinalSyncForDeployment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ErpBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ErpBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260425085128_FinalSyncForDeployment")]
+    partial class FinalSyncForDeployment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
---
+++ ErpBackend/Migrations/20260425085128_FinalSyncForDeployment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErpBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalSyncForDeployment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ExchangeRates",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2026, 4, 25, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "ExchangeRates",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2026, 4, 25, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ExchangeRates",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2026, 4, 23, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "ExchangeRates",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2026, 4, 23, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
